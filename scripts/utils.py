39a93403be32e88a8c4e4897728df07600928579
FrankieZJWZ <145820653+FrankieZJWZ@users.noreply.github.com>
2025-06-18 10:14:12 +0800

    Add files via upload

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,29 @@
+import calendar
+from datetime import datetime
+from datetime import timedelta
+import hashlib
+import os
+import re
+import requests
+import base64
+from weread2notionpro.config  import (
+    RICH_TEXT,
+    URL,
+    RELATION,
+    NUMBER,
+    DATE,
+    FILES,
+    STATUS,
+    TITLE,
+    SELECT,
+)
+import pendulum
+
+MAX_LENGTH = (
+    1024  # NOTION 2000个字符限制https://developers.notion.com/reference/request-limits
+)
+
+
 def get_heading(level, content):
     if level == 1:
         heading = "heading_1"
@@ -12,7 +38,7 @@ def get_heading(level, content):
                 {
                     "type": "text",
                     "text": {
-                        "content": content,
+                        "content": content[:MAX_LENGTH],
                     },
                 }
             ],
@@ -28,11 +54,11 @@ def get_table_of_contents():
 
 
 def get_title(content):
-    return {"title": [{"type": "text", "text": {"content": content}}]}
+    return {"title": [{"type": "text", "text": {"content": content[:MAX_LENGTH]}}]}
 
 
 def get_rich_text(content):
-    return {"rich_text": [{"type": "text", "text": {"content": content}}]}
+    return {"rich_text": [{"type": "text", "text": {"content": content[:MAX_LENGTH]}}]}
 
 
 def get_url(url):
@@ -47,10 +73,15 @@ def get_multi_select(names):
     return {"multi_select": [{"name": name} for name in names]}
 
 
-def get_date(start):
+def get_relation(ids):
+    return {"relation": [{"id": id} for id in ids]}
+
+
+def get_date(start, end=None):
     return {
         "date": {
             "start": start,
+            "end": end,
             "time_zone": "Asia/Shanghai",
         }
     }
@@ -75,7 +106,7 @@ def get_quote(content):
             "rich_text": [
                 {
                     "type": "text",
-                    "text": {"content": content},
+                    "text": {"content": content[:MAX_LENGTH]},
                 }
             ],
             "color": "default",
@@ -83,40 +114,252 @@ def get_quote(content):
     }
 
 
-def get_callout(content, style, colorStyle, reviewId):
-    # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
-    emoji = "〰️"
-    if style == 0:
-        emoji = "💡"
-    elif style == 1:
-        emoji = "⭐"
-    # 如果reviewId不是空说明是笔记
-    if reviewId != None:
-        emoji = "✍️"
+def get_block(content,type,show_color, style, colorStyle, reviewId):
     color = "default"
-    # 根据划线颜色设置文字的颜色
-    if colorStyle == 1:
-        color = "red"
-    elif colorStyle == 2:
-        color = "purple"
-    elif colorStyle == 3:
-        color = "blue"
-    elif colorStyle == 4:
-        color = "green"
-    elif colorStyle == 5:
-        color = "yellow"
-    return {
-        "type": "callout",
-        "callout": {
+    if show_color:
+        # 根据划线颜色设置文字的颜色
+        if colorStyle == 1:
+            color = "red"
+        elif colorStyle == 2:
+            color = "purple"
+        elif colorStyle == 3:
+            color = "blue"
+        elif colorStyle == 4:
+            color = "green"
+        elif colorStyle == 5:
+            color = "yellow"
+    block = {
+        "type": type,
+        type: {
             "rich_text": [
                 {
                     "type": "text",
                     "text": {
-                        "content": content,
+                        "content": content[:MAX_LENGTH],
                     },
                 }
             ],
-            "icon": {"emoji": emoji},
             "color": color,
         },
     }
+    if(type=="callout"):
+        # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
+        emoji = "〰️"
+        if style == 0:
+            emoji = "💡"
+        elif style == 1:
+            emoji = "⭐"
+        # 如果reviewId不是空说明是笔记
+        if reviewId != None:
+            emoji = "✍️"
+        block[type]["icon"] = {"emoji": emoji}
+    return block
+
+
+def get_rich_text_from_result(result, name):
+    return result.get("properties").get(name).get("rich_text")[0].get("plain_text")
+
+
+def get_number_from_result(result, name):
+    return result.get("properties").get(name).get("number")
+
+
+def format_time(time):
+    """将秒格式化为 xx时xx分格式"""
+    result = ""
+    hour = time // 3600
+    if hour > 0:
+        result += f"{hour}时"
+    minutes = time % 3600 // 60
+    if minutes > 0:
+        result += f"{minutes}分"
+    return result
+
+
+def format_date(date, format="%Y-%m-%d %H:%M:%S"):
+    return date.strftime(format)
+
+
+def timestamp_to_date(timestamp):
+    """时间戳转化为date"""
+    return datetime.utcfromtimestamp(timestamp) + timedelta(hours=8)
+
+
+def get_first_and_last_day_of_month(date):
+    # 获取给定日期所在月的第一天
+    first_day = date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+    # 获取给定日期所在月的最后一天
+    _, last_day_of_month = calendar.monthrange(date.year, date.month)
+    last_day = date.replace(
+        day=last_day_of_month, hour=0, minute=0, second=0, microsecond=0
+    )
+
+    return first_day, last_day
+
+
+def get_first_and_last_day_of_year(date):
+    # 获取给定日期所在年的第一天
+    first_day = date.replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+
+    # 获取给定日期所在年的最后一天
+    last_day = date.replace(month=12, day=31, hour=0, minute=0, second=0, microsecond=0)
+
+    return first_day, last_day
+
+
+def get_first_and_last_day_of_week(date):
+    # 获取给定日期所在周的第一天（星期一）
+    first_day_of_week = (date - timedelta(days=date.weekday())).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+
+    # 获取给定日期所在周的最后一天（星期日）
+    last_day_of_week = first_day_of_week + timedelta(days=6)
+
+    return first_day_of_week, last_day_of_week
+
+def get_properties(dict1, dict2):
+    properties = {}
+    for key, value in dict1.items():
+        type = dict2.get(key)
+        if value == None:
+            continue
+        property = None
+        if type == TITLE:
+            property = {
+                "title": [{"type": "text", "text": {"content": value[:MAX_LENGTH]}}]
+            }
+        elif type == RICH_TEXT:
+            property = {
+                "rich_text": [{"type": "text", "text": {"content": value[:MAX_LENGTH]}}]
+            }
+        elif type == NUMBER:
+            property = {"number": value}
+        elif type == STATUS:
+            property = {"status": {"name": value}}
+        elif type == FILES:
+            property = {
+                "files": [
+                    {"type": "external", "name": "Cover", "external": {"url": value}}
+                ]
+            }
+        elif type == DATE:
+            property = {
+                "date": {
+                    "start": pendulum.from_timestamp(
+                        value, tz="Asia/Shanghai"
+                    ).to_datetime_string(),
+                    "time_zone": "Asia/Shanghai",
+                }
+            }
+        elif type == URL:
+            property = {"url": value}
+        elif type == SELECT:
+            property = {"select": {"name": value}}
+        elif type == RELATION:
+            property = {"relation": [{"id": id} for id in value]}
+        if property:
+            properties[key] = property
+    return properties
+
+
+def get_property_value(property):
+    """从Property中获取值"""
+    type = property.get("type")
+    content = property.get(type)
+    if content is None:
+        return None
+    if type == "title" or type == "rich_text":
+        if len(content) > 0:
+            return content[0].get("plain_text")
+        else:
+            return None
+    elif type == "status" or type == "select":
+        return content.get("name")
+    elif type == "files":
+        # 不考虑多文件情况
+        if len(content) > 0 and content[0].get("type") == "external":
+            return content[0].get("external").get("url")
+        else:
+            return None
+    elif type == "date":
+        return str_to_timestamp(content.get("start"))
+    else:
+        return content
+
+
+
+
+def str_to_timestamp(date):
+    if date == None:
+        return 0
+    dt = pendulum.parse(date)
+    # 获取时间戳
+    return int(dt.timestamp())
+
+
+upload_url = "https://wereadassets.malinkang.com/"
+
+
+def upload_image(folder_path, filename, file_path):
+    # 将文件内容编码为Base64
+    with open(file_path, "rb") as file:
+        content_base64 = base64.b64encode(file.read()).decode("utf-8")
+
+    # 构建请求的JSON数据
+    data = {"file": content_base64, "filename": filename, "folder": folder_path}
+
+    response = requests.post(upload_url, json=data)
+
+    if response.status_code == 200:
+        print("File uploaded successfully.")
+        return response.text
+    else:
+        return None
+
+
+def url_to_md5(url):
+    # 创建一个md5哈希对象
+    md5_hash = hashlib.md5()
+
+    # 对URL进行编码，准备进行哈希处理
+    # 默认使用utf-8编码
+    encoded_url = url.encode("utf-8")
+
+    # 更新哈希对象的状态
+    md5_hash.update(encoded_url)
+
+    # 获取十六进制的哈希表示
+    hex_digest = md5_hash.hexdigest()
+
+    return hex_digest
+
+
+def download_image(url, save_dir="cover"):
+    # 确保目录存在，如果不存在则创建
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    file_name = url_to_md5(url) + ".jpg"
+    save_path = os.path.join(save_dir, file_name)
+
+    # 检查文件是否已经存在，如果存在则不进行下载
+    if os.path.exists(save_path):
+        print(f"File {file_name} already exists. Skipping download.")
+        return save_path
+
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(save_path, "wb") as file:
+            for chunk in response.iter_content(chunk_size=128):
+                file.write(chunk)
+        print(f"Image downloaded successfully to {save_path}")
+    else:
+        print(f"Failed to download image. Status code: {response.status_code}")
+    return save_path
+
+
+
+def get_embed(url):
+    return {"type": "embed", "embed": {"url": url}}
